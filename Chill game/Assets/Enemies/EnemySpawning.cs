7b90c46f582a11a705e6ed0772f3e4a7540d1422
Bob-Maximus <1elkabbanyou@hdsb.ca>
2026-05-01 11:04:15 -0400

    added EVIL enemy variants that spawn at night\

--- Chill game/Assets/Enemies/EnemySpawning.cs
+++ Chill game/Assets/Enemies/EnemySpawning.cs
@@ -10,14 +10,25 @@ public class EnemySpawning : MonoBehaviour
 
     public float mapWidth, mapHeight;
 
-    public List<EnemyEntry> enemies;
+    public List<EnemyEntry> enemiesDay;
+    public List<EnemyEntry> enemiesNight;
 
     void Update()
     {    
-        if (Random.Range(0, 500/(1+(time.round/100)))==1)
+        if (Random.Range(0, 500/(1+(time.round/100)))==2)
         {
-            int enemyIndex = Random.Range(0, enemies.Count);
-            Instantiate(enemies[enemyIndex].obj, new Vector3(Random.Range(-mapWidth/2, mapWidth/2), Random.Range(-mapHeight/2, mapHeight/2), enemies[enemyIndex].startHeight), transform.rotation);
+            Debug.Log("o");
+            if (time.day)
+            {
+                Debug.Log("d");
+                int enemyIndex = Random.Range(0, enemiesDay.Count);
+                Instantiate(enemiesDay[enemyIndex].obj, new Vector3(Random.Range(-mapWidth/2, mapWidth/2), Random.Range(-mapHeight/2, mapHeight/2), enemiesDay[enemyIndex].startHeight), transform.rotation);
+            } else
+            {
+                Debug.Log("n");
+                int enemyIndex = Random.Range(0, enemiesNight.Count);
+                Instantiate(enemiesNight[enemyIndex].obj, new Vector3(Random.Range(-mapWidth/2, mapWidth/2), Random.Range(-mapHeight/2, mapHeight/2), enemiesNight[enemyIndex].startHeight), transform.rotation);
+            }
         }
     }
 
